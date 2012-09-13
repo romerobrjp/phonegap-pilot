ba7c6c28db66463ec2b36afa5fbfa1a3b63261a9
Romero Meireles <romero.mfm@gmail.com>
2012-09-13 00:56:33 -0300

    Comecando a mexer no banco

--- assets/www/js/banco.js
+++ assets/www/js/banco.js
@@ -1,27 +1,86 @@
+
+
+function gerarTabelas(tx) {
+	//CRIANDO AS TABELAS
+	console.log("---transação rolando---");
+
+	
+	//PRODUTOS
+	tx.executeSql("CREATE TABLE IF NOT EXISTS PRODUTO " +
+			"(" +
+			"ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			"NOME VARCHAR(100), " +
+			"PRECO DECIMAL(10,2), " +
+			"CODIGO_BARRAS VARCHAR(100), " +
+			"IMAGEM BLOB" +
+			")");
+	
+	//PRECOS DOS PRODUTOS
+	tx.executeSql("CREATE TABLE IF NOT EXISTS PRODUTO_PRECOS " +
+			"(" +
+			"ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			"VALOR DECIMAL(10,2), " +
+			"DATA DATE, POSICAO VARCHAR(100)," +
+			"PRODUTO_ID INTEGER," +
+			"FOREIGN KEY(PRODUTO_ID) REFERENCES PRODUTO(ID)"
+			")");
+	
+	//POSICAO
+	tx.executeSql("CREATE TABLE IF NOT EXISTS PRECO_POCISAO " +
+			"(" +
+			"ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			"LONGITUDE REAL, " +
+			"LATITUDE REAL, " +
+			"PRECO_ID INTEGER, " +
+			"FOREIGN KEY(PRECO_ID) REFERENCES PRODUTO_PRECOS(ID)" +
+			")");
+	
+	console.log("---transação finalizada---");
+}
+
+function erro(e) {
+	console.log("transação erro");
+	alert(e);
+}
+
+function finallyy() {
+	console.log("transação final");
+}
+
 function setupDB() {
-	var db = window.openDatabase("pgp", "1.0", "phonegapProject", (1024 * 1024) * 5);
+	var db = window.openDatabase("phonegapProject", "1.0", "phonegapProject", (1024 * 1024) * 5);
 
 	if (db) {
 		console.log("---BANCO EXISTE---");
+		alert("---BANCO EXISTE---");
 		db.transaction(exec, erro, finallyy);
+		alert("---BANCO EXISTE---");
 	}
-}
-
-function exec(tx) {
-
-	console.log("---transação rolando---");
-
-	tx.executeSql("create table if not exists produto (LONGITUDE REAL, LATITUDE REAL");
-	
-	tx.executeSql("create table if not exists precosProduto (id integer Primary Key autoincrement, valor float(10,2), " +
-			"data date, posicao varchar(100))");
-	
-	tx.executeSql("create table if not exists produto (id integer Primary Key autoincrement, nome varchar(100), " +
-			"preco float(10,2), codigoBarras varchar(100), img varchar(255))");
-	
-	console.log("---transação realizada---");
 }
 
 $(function() {
 	setupDB();
-});
+	
+	var sql = "PRAGMA FOREIGN_KEYS";
+	
+	if (db) {
+		db.transaction(function(tx) {
+			tx.executeSql(
+					sql, 
+					[], 
+					function(tx, resultado) {
+						alert("Num resultados: " + resultado.rows.length);
+						
+						if (!resultSet.rowsAffected) {
+							  console.log('No rows affected!');
+							  alert('No rows affected!');
+							  return false;
+							}
+					}, 
+					function(err) {
+							alert('aguia geral na transaction' + +err.code);
+					}
+			)
+		)}
+	}
+}
