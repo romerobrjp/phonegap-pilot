12d378aaaac0ca07ccad6f81ab89d5c2e5780070
Manoel Amaro <mano.lira@gmail.com>
2012-09-15 16:02:19 -0300

    Fixed NewProduct page loading

--- assets/www/js/produtoNovo.js
+++ assets/www/js/produtoNovo.js
@@ -10,7 +10,7 @@ function tirarFoto() {
 		{
 			quality: 50,
 			targetWidth: 120,
-			navigator.camera.DestinationType: Camera.DestinationType.DATA_URL
+			destinationType: Camera.DestinationType.DATA_URL
 		}
 	);
 }
@@ -26,69 +26,60 @@ function lerCodigoBarras() {
 	);
 }
 
+
+
 function produtoCadastrar(nome, codigo_barras, imagem, preco, longi, lati) {
 	if (db) {
 		var sql = "INSERT INTO PRODUTO (NOME, CODIGO_BARRAS, IMAGEM) VALUES ('" + nome + "', '" + codigo_barras + "', '" + imagem + "')";
-		
 		db.transaction(
-			function(tx) {
-				alert("INICIANDO INSERT");
-				tx.executeSql(sql);
-				alert("EXECUTOU INSERT");
-			},
-			function errorCB(err) {
-			    alert("Erro ao inserir Produto: " + err.code + ' - ' + err.message);
-			    return false;
-			},
-			function successCB() {
-			    alert("Sucesso ao inserir Produto!");
-			    return true;
-			}
-		);
+				function(tx) {
+					alert("INICIANDO INSERT");
+					tx.executeSql(sql);
+					alert("EXECUTOU INSERT");
+				},
+				function errorCB(err) {
+					alert("Erro ao inserir Produto: " + err.code + ' - ' + err.message);
+					return false;
+				},
+				function successCB() {
+					alert("Sucesso ao inserir Produto!");
+					return true;
+				});
 	}
 	
 	var produtoId = pesquisarProdutoIdPorCodigoBarras(codigo_barras);
-	
 	if (db) {
-		var sql = "INSERT INTO PRODUTO_PRECOS (PRECO, DATA, LONGITUDE, LATITUDE, PRODUTO_ID) VALUES (" + preco + ", select date('now'), " + longi + ", " + lati + ", " + produtoId + ")";
-		
+		var sql = "INSERT INTO PRODUTO_PRECOS (PRECO, DATA, LONGITUDE, LATITUDE, PRODUTO_ID) VALUES (" + preco + ", date('now'), " + longi + ", " + lati + ", " + produtoId + ")";
 		db.transaction(
-			function(tx) {
-				tx.executeSql(sql);
-			}
-		);
+				function(tx) {
+					tx.executeSql(sql);
+				});
 	}
-	alert("Produto cadastrado com sucesso!");
 	$.mobile.changePage( "index.html", { transition: "slideup"} );
 }
 
 function pesquisarProdutoIdPorCodigoBarras(codigo_barras) {
 	if (db) {
-		db.transaction(
-			function(tx) {
-				var sql = "SELECT * FROM PRODUTO WHERE CODIGO_BARRAS LIKE '" + codigo_barras + "'";				
-				tx.executeSql(
+		db.transaction(function(tx) {
+			var sql = "SELECT * FROM PRODUTO WHERE CODIGO_BARRAS='" + codigo_barras + "'";
+			tx.executeSql(
 					sql, 
 					[], 
 					function consultaSucesso(tx, results) {
 						var len = results.rows.length;
-					    for (var i=0; i<len; i++) {
-					        return results.rows.item(i).ID;
-					    }
+						for (var i=0; i<len; i++) {
+							return results.rows.item(i).ID;
+						}
 					},
 					function(err) {
 						alert('Erro no executeSQL: ' + err.code + ' - ' + err.message);
-					}
-				)
-			},
-			function errorCB(err) {
-			    alert("Erro no db.transaction: " + err.code + ' - ' + err.message);
-			    return false;
-			},
-			function successCB() {
-			    console.log("Sucesso na consulta de ID por Codgio de Barras!");
-			    return true;
-			}
-		);	
+					})
+				}, function errorCB(err) {
+					alert("Erro no db.transaction: " + err.code + ' - ' + err.message);
+					return false;
+				}, function successCB() {
+					console.log("Sucesso na consulta de ID por Codgio de Barras!");
+					return true;
+				});
 	}
 }
